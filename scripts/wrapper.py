e69e5d594532b2b679daf6f8140cdcc4ec2e237b
Felix2048 <infinitesamsarax@outlook.com>
2021-04-14 12:04:53 +0800

    Add wrapper

---
+++ scripts/wrapper.py
@@ -0,0 +1,18 @@
+from MatterSim import Simulator 
+
+class SimulatorWrapper(Simulator):
+
+    def __init__(self):
+        super(Simulator, self).__init__()
+
+    def init(self):
+        super(Simulator, self).initialize()
+
+    def newEpisode(self, scanId, viewpointId, heading, elevation):
+        super(Simulator, self).newEpisode([scanId], [viewpointId], [heading], [elevation])
+
+    def getState(self, scanId, viewpointId, heading, elevation):
+        return super(Simulator, self).getState()[0]
+
+    def makeAction(self, index, heading, elevation):
+        super(Simulator, self).makeAction([index], [heading], [elevation])
