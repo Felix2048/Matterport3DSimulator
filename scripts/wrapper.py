8c7bcf43fcabe024d47f88867b640a14081d6547
Felix2048 <infinitesamsarax@outlook.com>
2021-04-15 20:50:23 +0800

    Update wrapper

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -8,7 +8,7 @@ class SimulatorWrapper(Simulator):
     def newEpisode(self, scanId, viewpointId, heading, elevation):
         super(SimulatorWrapper, self).newEpisode([scanId], [viewpointId], [heading], [elevation])
 
-    def getState(self, scanId, viewpointId, heading, elevation):
+    def getState(self):
         return super(SimulatorWrapper, self).getState()[0]
 
     def makeAction(self, index, heading, elevation):
