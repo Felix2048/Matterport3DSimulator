d055be79b5c58fe12aec0cd727250e653020f754
Felix2048 <infinitesamsarax@outlook.com>
2021-04-29 20:45:51 +0800

    Update download.sh

--- tasks/R2R/data/download.sh
+++ tasks/R2R/data/download.sh
@@ -4,3 +4,5 @@ wget https://www.dropbox.com/s/hh5qec8o5urcztn/R2R_train.json -P tasks/R2R/data/
 wget https://www.dropbox.com/s/8ye4gqce7v8yzdm/R2R_val_seen.json -P tasks/R2R/data/
 wget https://www.dropbox.com/s/p6hlckr70a07wka/R2R_val_unseen.json -P tasks/R2R/data/
 wget https://www.dropbox.com/s/w4pnbwqamwzdwd1/R2R_test.json -P tasks/R2R/data/
+wget http://people.eecs.berkeley.edu/~ronghang/projects/speaker_follower/data_augmentation/R2R_data_augmentation_paths.json -P tasks/R2R/data/
+
